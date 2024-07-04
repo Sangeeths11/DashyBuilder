7d87c128b6ae84f8e3cfc4c7cdb8f92ae6bc9e2f
Sangeeths11 <c.sangeeths@gmail.com>
2024-07-04 10:45:41 +0200

    refactor: Docker File created. Deployment finished

--- DashyBuilder.Backend/app.py
+++ DashyBuilder.Backend/app.py
@@ -1,22 +1,10 @@
-from flask import Flask, request, make_response
-from flask_cors import CORS
-import json
-
+from flask import Flask
 app = Flask(__name__)
-CORS(app, expose_headers=['Content-Disposition'])
 
-def generate_plotly_code(widgets):
-    return 
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
-@app.route('/export', methods=['POST'])
-def export_dashboard():
-    widgets = request.get_json()
-    python_code = generate_plotly_code(widgets)
 
-    response = make_response(python_code)
-    response.headers['Content-Disposition'] = 'attachment; filename=dashboard.py'
-    response.headers['Content-Type'] = 'text/plain'
-    return response
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
---
+++ DashyBuilder.Frontend/Dockerfile
@@ -0,0 +1,33 @@
+# syntax = docker/dockerfile:1
+
+ARG NODE_VERSION=18.14.2
+
+FROM node:${NODE_VERSION}-slim as base
+
+ARG PORT=3000
+
+ENV NODE_ENV=production
+
+WORKDIR /src
+
+# Build
+FROM base as build
+
+COPY --link package.json package-lock.json .
+RUN npm install --production=false
+
+COPY --link . .
+
+RUN npm run build
+RUN npm prune
+
+# Run
+FROM base
+
+ENV PORT=$PORT
+
+COPY --from=build /src/.output /src/.output
+# Optional, only needed if you rely on unbundled dependencies
+# COPY --from=build /src/node_modules /src/node_modules
+
+CMD [ "node", ".output/server/index.mjs" ]
