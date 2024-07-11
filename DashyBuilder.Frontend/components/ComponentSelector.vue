ef77f0dc64ad4da156e0a08a14cf5aa39200d26f
Sangeeths11 <c.sangeeths@gmail.com>
2024-07-11 11:16:44 +0200

    chore: Disable grid size selection for existing projects

--- DashyBuilder.Frontend/components/ComponentSelector.vue
+++ DashyBuilder.Frontend/components/ComponentSelector.vue
@@ -12,11 +12,11 @@
         <label class="block text-gray-700 text-sm font-bold mb-2">Grid Size</label>
         <div class="flex items-center">
           <label class="inline-flex items-center mr-4">
-            <input type="radio" class="form-radio text-blue-600" name="gridSize" value="3x3" v-model="gridSize">
+            <input type="radio" class="form-radio text-blue-600" name="gridSize" value="3x3" v-model="gridSize" :disabled="isGridSizeDisabled">
             <span class="ml-2">3x3 <Icon name="mdi:grid" class="ml-1 w-7 h-7 text-blue-600"/></span>
           </label>
           <label class="inline-flex items-center">
-            <input type="radio" class="form-radio text-blue-600" name="gridSize" value="4x4" v-model="gridSize">
+            <input type="radio" class="form-radio text-blue-600" name="gridSize" value="4x4" v-model="gridSize" :disabled="isGridSizeDisabled">
             <span class="ml-2">4x4 <Icon name="material-symbols-light:background-grid-small-outline-sharp" class="ml-1 w-10 h-10 text-blue-600"/></span>
           </label>
         </div>
@@ -40,6 +40,18 @@ const components = ref([
 ]);
 const selectedComponent = ref('');
 const gridSize = ref('3x3');
+const isGridSizeDisabled = ref(false);
+const route = useRoute();
+const projectStore = useProjectStore();
+
+onMounted(async () => {
+  const projectId = route.params.id;
+  const projectGridSize = await projectStore.fetchProjectGridSize(projectId);
+  if (projectGridSize) {
+    gridSize.value = projectGridSize;
+    isGridSizeDisabled.value = true;
+  }
+});
 
 function addWidget() {
   if (selectedComponent.value) {
@@ -49,7 +61,6 @@ function addWidget() {
       gridSize: gridSize.value
     });
     selectedComponent.value = '';
-    gridSize.value = '3x3';
   }
 }
 </script>
