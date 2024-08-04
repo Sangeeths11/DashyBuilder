3300b221bd1d12135aeab0ee7aa1c6b08375b8a1
Sangeeths11 <c.sangeeths@gmail.com>
2024-08-04 18:53:15 +0200

    feat: Add TableWidget class for displaying tables in the dashboard

---
+++ DashyBuilder.Backend/components/widgets/table.py
@@ -0,0 +1,14 @@
+from components.widgets.base import Widget
+
+class TableWidget(Widget):
+    def generate_code(self):
+        return f"""
+def drawTable_{self.name}():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dbc.Table.from_dataframe(table_df, striped=True, bordered=True, hover=True, dark=True, responsive=True, style={{'width': '100%', 'overflowY': 'auto'}})
+            ]), style={{'height': '100%', 'overflow': 'hidden'}}
+        )
+    ], style={{'height': '100%', 'padding': '2px'}})
+"""
