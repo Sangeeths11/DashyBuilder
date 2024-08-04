f524f46b2bb8c95c952c83455a0eda48a8727695
Sangeeths11 <c.sangeeths@gmail.com>
2024-08-04 18:53:03 +0200

    feat: Add FilterBoxWidget class for displaying a filter box in the dashboard

---
+++ DashyBuilder.Backend/components/widgets/filter_box.py
@@ -0,0 +1,20 @@
+from components.widgets.base import Widget
+
+class FilterBoxWidget(Widget):
+    def generate_code(self):
+        return f"""
+def drawFilterBox_{self.name}():
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dcc.Dropdown(
+                    id='{self.name}_dropdown',
+                    options=[{{'label': 'Setosa', 'value': 'setosa'}}, {{'label': 'Versicolor', 'value': 'versicolor'}}, {{'label': 'Virginica', 'value': 'virginica'}}],
+                    value=['setosa', 'versicolor', 'virginica'],
+                    multi=True,
+                    style={{'color': '#000'}}
+                )
+            ]), style={{'height': '100%'}}
+        )
+    ], style={{'height': '100%', 'padding': '2px'}})
+"""
