f218fe7aaa1a2fad76419291bf53f3624a51fefd
Sangeeths11 <c.sangeeths@gmail.com>
2024-08-04 18:52:51 +0200

    feat: Add ChartWidget class for displaying charts in the dashboard

---
+++ DashyBuilder.Backend/components/widgets/chart.py
@@ -0,0 +1,26 @@
+from components.widgets.base import Widget
+
+class ChartWidget(Widget):
+    def generate_code(self):
+        return f"""
+def drawChart_{self.name}():
+    fig = px.bar(df, x='sepal_width', y='sepal_length', color='species').update_layout(
+        template='plotly_dark',
+        plot_bgcolor='rgba(0, 0, 0, 0)',
+        paper_bgcolor='rgba(0, 0, 0, 0)',
+        margin=dict(l=20, r=20, t=20, b=20)
+    )
+    return html.Div([
+        dbc.Card(
+            dbc.CardBody([
+                dcc.Graph(
+                    id='{self.name}_graph',
+                    figure=fig,
+                    config={{'displayModeBar': False}},
+                    style={{'height': '100%', 'width': '100%'}},
+                    responsive=True
+                )
+            ]), style={{'height': '100%'}}
+        )
+    ], style={{'height': '100%', 'padding': '2px'}})
+"""
