45566f2d71d25612b04d6a61d14e12c1d4ca3ce3
Sangeeths11 <c.sangeeths@gmail.com>
2024-08-13 13:42:25 +0200

    refactor: Update ChartWidget with title

--- DashyBuilder.Backend/components/widgets/chart.py
+++ DashyBuilder.Backend/components/widgets/chart.py
@@ -6,10 +6,11 @@ class ChartWidget(Widget):
         return f"""
 def drawChart_{self.name}():
     fig = px.pie(df, names='species', values='sepal_length').update_layout(
+        title={{'text': 'Chart Title: {self.name}', 'y':0.95, 'x':0.01, 'xanchor': 'left', 'yanchor': 'top'}},
         template='plotly_dark',
         plot_bgcolor='rgba(0, 0, 0, 0)',
         paper_bgcolor='rgba(0, 0, 0, 0)',
-        margin=dict(l=20, r=20, t=20, b=20)
+        margin=dict(l=20, r=20, t=40, b=20)
     )
     return html.Div([
         dbc.Card(
