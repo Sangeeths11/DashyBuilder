a158ff2750a1a201d8e42a777bda5c56e1574e51
Sangeeths11 <c.sangeeths@gmail.com>
2024-08-29 16:41:20 +0200

    refactor: Update configComp store to remove unused imports and improve code readability

---
+++ DashyBuilder.Frontend/stores/configComp.ts
@@ -0,0 +1,13 @@
+export const useConfigCompStore = defineStore('configComp', () => {
+    const client = useSupabaseClient();
+    const user = useSupabaseUser();
+  
+  
+    const errorMessages = ref([]);
+  
+  
+    return {
+      
+    };
+  });
+  
