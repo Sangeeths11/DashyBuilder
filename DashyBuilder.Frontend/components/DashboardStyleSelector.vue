122cfe4127354a2a3b622ee42b2ed2248d74017b
Adrian Cerdeira <acwebdesign01@gmail.com>
2025-02-13 17:50:15 +0100

    fix: flex style on select

--- DashyBuilder.Frontend/components/DashboardStyleSelector.vue
+++ DashyBuilder.Frontend/components/DashboardStyleSelector.vue
@@ -2,7 +2,7 @@
   <div class="w-full mb-5">
     <div class="bg-white shadow-lg rounded-lg p-4 h-full flex flex-col">
       <h2 class="font-bold text-lg mb-4">Dashboard Options</h2>
-      <div class="flex flex-col w-full lg:w-1/2">
+      <div class="flex flex-col">
         <label for="mockup-style" class="block text-gray-700 text-sm font-bold mb-2">Style</label>
         <select id="mockup-style" v-model="selectedMockupStyle" class="p-2 border rounded w-full"
           @change="emitSelectedStyle">
