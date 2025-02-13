de9fe633ef6fab0ed2bf8b425b879af56c450b50
Adrian Cerdeira <acwebdesign01@gmail.com>
2025-02-13 18:23:01 +0100

    style: add simpler border and icon to style select

--- DashyBuilder.Frontend/components/DashboardStyleSelector.vue
+++ DashyBuilder.Frontend/components/DashboardStyleSelector.vue
@@ -3,8 +3,11 @@
     <div class="bg-white shadow-lg rounded-lg p-4 h-full flex flex-col">
       <h2 class="font-bold text-lg mb-4">Dashboard Options</h2>
       <div class="flex flex-col">
-        <label for="mockup-style" class="block text-gray-700 text-sm font-bold mb-2">Style</label>
-        <select id="mockup-style" v-model="selectedMockupStyle" class="p-2 border rounded w-full"
+        <label for="mockup-style" class="text-gray-700 text-sm font-bold mb-2 flex align-middle">
+          <Icon name="mdi:color" class="text-xl" /> Style
+        </label>
+        <select id="mockup-style" v-model="selectedMockupStyle"
+          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2"
           @change="emitSelectedStyle">
           <option v-for="mockupStyle in mockupStyles" :key="mockupStyle.id" :value="mockupStyle.id">
             {{ mockupStyle.name }}
