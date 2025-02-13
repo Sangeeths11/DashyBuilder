154795a5588e70e7fc3b893eda92e8ee1ac34bca
Adrian Cerdeira <acwebdesign01@gmail.com>
2025-02-13 17:47:43 +0100

    feat: create dashboard style selector and upload mockup field

---
+++ DashyBuilder.Frontend/components/DashboardStyleSelector.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="w-full mb-5">
+    <div class="bg-white shadow-lg rounded-lg p-4 h-full flex flex-col">
+      <h2 class="font-bold text-lg mb-4">Dashboard Options</h2>
+      <div class="flex flex-col w-full lg:w-1/2">
+        <label for="mockup-style" class="block text-gray-700 text-sm font-bold mb-2">Style</label>
+        <select id="mockup-style" v-model="selectedMockupStyle" class="p-2 border rounded w-full"
+          @change="emitSelectedStyle">
+          <option v-for="mockupStyle in mockupStyles" :key="mockupStyle.id" :value="mockupStyle.id">
+            {{ mockupStyle.name }}
+          </option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['selected-style']);
+const mockupStyles = ref([
+  { id: 'components', name: 'Put own dashboard with components together' },
+  { id: 'mockup', name: 'Upload dashboard mockup' },
+]);
+const selectedMockupStyle = ref('components');
+
+function emitSelectedStyle() {
+  if (selectedMockupStyle.value) {
+    emit('selected-style', selectedMockupStyle.value);
+  }
+}
+</script>
--- DashyBuilder.Frontend/pages/overview/[id].vue
+++ DashyBuilder.Frontend/pages/overview/[id].vue
@@ -1,33 +1,23 @@
 <template>
   <div>
     <h1 class="text-3xl font-bold mb-6">{{ name }}</h1>
-    <SucessMessageBox :message="successMessage"/>
-    <ErrorMessageBox :message="errorMessage"/>
+    <SucessMessageBox :message="successMessage" />
+    <ErrorMessageBox :message="errorMessage" />
     <div class="flex flex-wrap mb-5">
       <div class="w-full lg:w-1/2 px-2 flex">
-        <UploadDataset 
-          @uploaded="handleDatasetUploaded" 
-          @loading="loadingData = $event" 
-          class="mb-5 flex-grow"
-          :projectId="projectId"
-        />
+        <UploadDataset @uploaded="handleDatasetUploaded" @loading="loadingData = $event" class="mb-5 flex-grow"
+          :projectId="projectId" />
       </div>
-      <div class="w-full lg:w-1/2 px-2 flex">
-        <ComponentSelector 
-          @add-widget="handleAddWidget" 
-          @errorMessage="errorMessageModal" 
-          class="mb-5 flex-grow"
-        />
+      <div class="w-full lg:w-1/2 px-2 flex flex-col">
+        <DashboardStyleSelector @selected-style="handleMockupStyle" />
+        <UploadMockup @uploaded="handleMockupUploaded" @loading="loadingMockup = $event" class="mb-5 flex-grow"
+          :projectId="projectId" v-if="showUploadMockup" />
+        <ComponentSelector @add-widget="handleAddWidget" @errorMessage="errorMessageModal" class="w-full"
+          v-if="!showUploadMockup" />
       </div>
     </div>
-    <DashboardArea 
-      :widgets="widgetStore.widgets" 
-      :gridSize="gridSize"
-      :uploadedDatasetId="uploadedDatasetId"
-      :projectId="projectId" 
-      @delete-widget="handleDeleteWidget" 
-      @update-widget="handleUpdateWidget" 
-    />
+    <DashboardArea :widgets="widgetStore.widgets" :gridSize="gridSize" :uploadedDatasetId="uploadedDatasetId"
+      :projectId="projectId" @delete-widget="handleDeleteWidget" @update-widget="handleUpdateWidget" />
   </div>
 </template>
 
@@ -47,7 +37,10 @@ const errorMessage = ref('');
 const successMessage = ref('');
 const gridSize = ref('');
 const uploadedDatasetId = ref(null);
+const uploadedMockupId = ref(null);
 const loadingData = ref(false);
+const loadingMockup = ref(false);
+const showUploadMockup = ref(false);
 
 watch(projectId, async (newId, oldId) => {
   if (newId !== oldId) {
@@ -85,7 +78,7 @@ const errorMessageModal = (message) => {
   errorMessage.value = message;
   setTimeout(() => {
     errorMessage.value = '';
-    }, 3000);
+  }, 3000);
 };
 
 const handleDeleteWidget = async (id) => {
@@ -121,9 +114,35 @@ const handleDatasetUploaded = async (datasetId) => {
   }
 };
 
+const handleMockupUploaded = async (datasetId) => {
+  uploadedMockupId.value = datasetId;
+  loadingMockup.value = true;
+
+  try {
+    const response = await fetch(`http://127.0.0.1:5000/data/${datasetId}`);
+    if (response.ok) {
+    } else {
+      errorMessage.value = 'Fehler beim Laden des Mockups.';
+    }
+  } catch (error) {
+    errorMessage.value = 'Fehler beim Laden des Mockups: ' + error.message;
+  } finally {
+    loadingMockup.value = false;
+  }
+};
+
+const handleMockupStyle = async (showUploadMockupId) => {
+  if (showUploadMockupId === 'mockup') {
+    showUploadMockup.value = true;
+  } else {
+    showUploadMockup.value = false;
+  }
+}
+
 onMounted(async () => {
   await widgetStore.fetchWidgetsByProjectId(projectId.value);
   uploadedDatasetId.value = await projectStore.fetchProjectFilePath(projectId.value);
+  uploadedMockupId.value = await projectStore.fetchProjectFilePath(projectId.value);
 });
 </script>
 
